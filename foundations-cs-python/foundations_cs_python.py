b598b7deb0f3e8b706067741282895daa0c23000
Ali Hamieh <hamieh8888@gmail.com>
2023-10-29 21:05:41 +0200

    mewnu

--- foundations-cs-python/foundations_cs_python.py
+++ foundations-cs-python/foundations_cs_python.py
@@ -1,4 +1,5 @@
+def displaylist():
+    
+ print ("press 1 for Add matrices \n" + "2 to check rotation \n " + "3 invert dictionary \n" + " 4 to convert Matrix to Dictionary \n")
+    
 
-ghkgfhgfj
-
-starting
