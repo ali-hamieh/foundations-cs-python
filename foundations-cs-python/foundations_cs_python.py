feac5e51423cad790fd24b67399928ae930dcf45
Ali Hamieh <hamieh8888@gmail.com>
2023-10-29 21:33:33 +0200

    welcome name

--- foundations-cs-python/foundations_cs_python.py
+++ foundations-cs-python/foundations_cs_python.py
@@ -2,11 +2,14 @@ def List():
     
  print ("press \n1 for Add matrices \n" + "2 to check rotation \n" +"3 invert dictionary \n" + "4 to convert Matrix to Dictionary \n" + "5 to Check Palindrom \n" + "5 search for element \n" + "7 to exit \n" )
  
+def welcome():
 
+ name = input(print("please enter your name"))
+ print("welcome \t" + name) 
 
 def main():
   
- 
+    welcome()
     List()
    
 main()
