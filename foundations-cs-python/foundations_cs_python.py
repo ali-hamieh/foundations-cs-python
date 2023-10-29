570b22b20daf67bbbcef4884c074240d19c1a5e7
Ali Hamieh <hamieh8888@gmail.com>
2023-10-29 21:20:55 +0200

    list finished

--- foundations-cs-python/foundations_cs_python.py
+++ foundations-cs-python/foundations_cs_python.py
@@ -1,5 +1,14 @@
-def displaylist():
+def List():
     
- print ("press 1 for Add matrices \n" + "2 to check rotation \n " + "3 invert dictionary \n" + " 4 to convert Matrix to Dictionary \n")
+ print ("press \n1 for Add matrices \n" + "2 to check rotation \n" +"3 invert dictionary \n" + "4 to convert Matrix to Dictionary \n" + "5 to Check Palindrom \n" + "5 search for element \n" + "7 to exit \n" )
+ 
+
+
+def main():
+  
+ 
+    List()
+   
+main()
     
 
