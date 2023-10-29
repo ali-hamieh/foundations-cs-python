94d22d7352d95c0a97ccee06c3f6ecfe4d5ea6df
Ali Hamieh <hamieh8888@gmail.com>
2023-10-30 00:21:09 +0200

    working

--- foundations-cs-python/foundations_cs_python.py
+++ foundations-cs-python/foundations_cs_python.py
@@ -11,36 +11,40 @@ def welcome():
  print("welcome \t" + name) 
  
 def matrixSum():
-  m1 = []  
+   m1 = []  
   m2 = []
   m3 = []
 
   row1 = int(input(print("enter number of row")))
   col1 = int(input(print ("enter number of column")))
 
-    for row in range(row1):
-      print("row", row)
-      m1.append([])
-      for col in range(col1):
-       print("column", col)
-       number = int(input("enter numbers of first column"))
-       m1[row].append(number)
-     print(m1)    
+  for row in range(row1):
+   print("row", row)
+   m1.append([])
+   for col in range(col1):
+     print("column", col)
+     number = int(input("enter numbers "))
+     m1[row].append(number)
+   print(m1)    
 
-     for row in range(row1):
-       print("row", row)
-        m2.append([])
-      for col in range(col1):
-       print("column", col)
-       number = int(input("enter numbers of first column"))
-       m2[row].append(number)
-     print(m2 )
+  for row in range(row1):
+   print("row", row)
+   m2.append([])
+   for col in range(col1):
+    print("column", col)
+    number = int(input("enter numbers "))
+    m2[row].append(number)
+  print(m2)    
 
 
-      for row in range(len(m1)):
-       for col in range(len(m2)):
-       m3[row][col] = m1[row][col] + m2[row][col]
+  for row in range(row1):
+   m3.append([])
+   for col in range(col1):
+    inx = m1[row][col] + m2[row][col]
+    m3[row].append(inx)
 
+
+  print(m3)
       
   
 
