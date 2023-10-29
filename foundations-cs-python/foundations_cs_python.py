55a39e5d46ee711ab51f3f586608c32e8b0bcb38
Ali Hamieh <hamieh8888@gmail.com>
2023-10-29 22:06:18 +0200

    fixing errors

--- foundations-cs-python/foundations_cs_python.py
+++ foundations-cs-python/foundations_cs_python.py
@@ -1,3 +1,6 @@
+from token import NUMBER
+
+
 def List():
     
  print ("press \n1 for Add matrices \n" + "2 to check rotation \n" +"3 invert dictionary \n" + "4 to convert Matrix to Dictionary \n" + "5 to Check Palindrom \n" + "5 search for element \n" + "7 to exit \n" )
@@ -7,22 +10,26 @@ def welcome():
  name = input(print("please enter your name"))
  print("welcome \t" + name) 
  
-def matrixSum(m1,m2)
+def matrixSum()
   m1 = []  
   m2 = []
   row = int(input(print"enter number of row"))
   column = int(input(print"enter number of column"))
   
   for i in range(row):
-    print("row", row)
+    print("row", i)
     m1.append([])
     for y in range(column):
       print("column", column)
+      numbwr = eval(input("enter numbers of first column"))
+      m1[row].append(NUMBER)
+  print(m1)    
       
   
 
 def main():
-  
+    
+    matrixSum()
     welcome()
     List()
    
