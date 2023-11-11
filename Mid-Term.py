94cfb3e397617cf122c89937b7e3852e807a1f21
Ali Hamieh <hamieh8888@gmail.com>
2023-11-11 10:43:49 +0200

    main

--- Mid-Term.py
+++ Mid-Term.py
@@ -15,10 +15,16 @@
 
 
 def displayMenu():
-    rrint("1.\n"  +
-          "2.\n"   +
-          "3.\n"  +
-           "4.\n" +
-          "5.\n"  +
-          "6.\n"
-          "")
+    rrint("1.Open Tab\n"  +
+          "2.Cose Tab\n"   +
+          "3.Switch Tab\n"  +
+          "4.Display All Tabs\n" +
+          "5.Open Nester Tabs\n"  +
+          "6.Clear All Tabs\n"  +
+          "7.Save Tabs\n"  +
+          "8.Import Tabs\n"  +
+          "9.Exit\n"  +)
+    
+
+def main():
+    displayMenu()
