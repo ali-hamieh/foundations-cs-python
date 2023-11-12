f60d00c6c720abb700a8475067503592dd58e5bb
Ali Hamieh <hamieh8888@gmail.com>
2023-11-12 11:05:07 +0200

    question 8 linput

--- Mid-Term.py
+++ Mid-Term.py
@@ -2,6 +2,13 @@ import urllib.request
 import json
 
 def load():
+    dict = {}
+    z = int(input(print("enter number of tabs ")))
+    for i in range(z):
+        x = input(print("enter website"))
+        y = input(print("enter url"))
+        dict[x] = [y]
+    print(dict)
     
 def jSon():
     dict = {}
