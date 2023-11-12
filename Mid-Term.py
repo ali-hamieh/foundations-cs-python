f183e0cde984db0294a775235e1f64bcd7a1722b
Ali Hamieh <hamieh8888@gmail.com>
2023-11-12 09:53:17 +0200

    question 6

--- Mid-Term.py
+++ Mid-Term.py
@@ -1,4 +1,15 @@
 import urllib.request
+def clearAll():
+    dict = {}
+    z = int(input(print("enter number of tabs ")))
+    for i in range(z):
+        x = input(print("enter website"))
+        y = input(print("enter url"))
+        dict[x] = [y]
+    print(dict)
+    dict.clear()
+    print(dict)
+
 
 def printIndex():
     dict = {}
