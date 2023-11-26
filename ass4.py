933d3ceead53fb14a882689de6604a5c0ccf3a26
Ali Hamieh <hamieh8888@gmail.com>
2023-11-26 18:45:00 +0200

    add node function

--- ass4.py
+++ ass4.py
@@ -11,7 +11,14 @@ class LinkedList:
     self.head = None
     self.size = 0
 
-      
+
+def addNode(self, data):
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
+    self.size += 1
+
+    
 def welcome():
   name = input(print("enter your name"))
   print("welcome" + name)
