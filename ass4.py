8aac94229e152350d48ea794fa8f9a8dc1017f97
Ali Hamieh <hamieh8888@gmail.com>
2023-11-27 15:34:33 +0200

    ex4

--- ass4.py
+++ ass4.py
@@ -111,6 +111,14 @@ def main():
           singlyLinked()
       elif choice == 2:
           pass
+      elif choice == 3:
+          pass
+      elif choice == 4:
+          pass
+      elif choice == 5 :
+          pass
+      elif choice == 6:
+          pass
 
 
 
